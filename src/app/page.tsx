4e6a0d2d37ad1710284711bbad45f02ec2af47f3
Harshxgit <htp.codes@gmail.com>
2024-12-23 15:53:17 +0530

    created side components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import Navbar from "@/componets/Navbar";
+import Services from "@/componets/Services";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <div className="text-red-600 text-7xl grid grid-rows-3 grid-flow-col gap-4 h-screen">
-      <div className="bg-red-400  md:row-span-3">1</div>
-      <div className="bg-cyan-400 md:col-span-2  ">2</div>
-      <div className="border-l-orange-950 grid md:grid-flow-col md:row-span-2 md:col-span-2  gap-4">
-        <div className="">1</div>
-        <div className="">2</div>
+      <div className=" text-7xl grid grid-rows-3 grid-flow-col gap-4 h-screen">
+        <div className="  md:row-span-3 border-2 max-w-[400px]  ">
+          <Services />
+        </div>
+        <div className=" md:col-span-2  border-2 ">2</div>
+        <div className="border-l-orange-950 grid md:grid-flow-col md:row-span-2 md:col-span-2  gap-4">
+          <div className="border-2 ">1</div>
+          <div className="border-2 ">2</div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
---
+++ src/componets/Services.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div className="m-4 top-32  sticky border-2">
+      <div className="text-3xl relative mt-2">
+        <h1>Select a Service</h1>
+        {/* <div className=" absolute top-2 right-4 text-white h-2 w-80"/> */}
+      </div>
+
+      {/* service container  */}
+      <div className="flex items-center justify-center m-2 gap-10 flex-wrap text-sm w-fit h-fit">
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+        <div className="w-20 h-20 border-2 rounded-lg flex items-center justify-center flex-col ">
+          {/* <img src="" alt="" /> */} img
+          <div className="">service name</div>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
