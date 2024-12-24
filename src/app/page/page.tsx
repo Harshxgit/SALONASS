2f27c4bda7f19d416451439e2d0dedc9b14b181f
Harshxgit <htp.codes@gmail.com>
2024-12-24 16:48:15 +0530

    fixe drawer

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,7 +1,10 @@
+import MyDrawer from '@/componets/MyDrawer'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <MyDrawer/>
+    </div>
   )
 }
