3f8876db5fff82db01ecc6ce16df76db21865748
Harshxgit <htp.codes@gmail.com>
2024-12-29 20:48:46 +0530

    s

--- src/app/actions/booking/index.ts
+++ src/app/actions/booking/index.ts
@@ -3,24 +3,49 @@ interface Booking {
   userid: number;
   services: { name: string; price: number; quantity: number }[];
   address: string;
+  bookingtype: string;
+  price: number;
+  date: Date;
+  time: string;
 }
-export default function addBooking({ userid, services, address }: Booking) {
-    try {
-        const booking = prisma.booking.create({
+export default function addBooking({
+  userid,
+  price,
+  services,
+  address,
+  bookingtype,
+  date,
+  time,
+}: Booking) {
+  try {
+    const isbooking = prisma.$transaction(async (tx) => {
+      const booking = await tx.booking.create({
         data: {
-            userid,
-            services: services.map((service) => ({
+          userId: userid,
+          bookingType: bookingtype,
+          address: address,
+          price: price,
+          date: date,
+          time: time,
+        },
+      });
+
+      const bookingid = booking.id;
+
+      tx.bookedService.create({
+        data: {
+          bookingId: bookingid,
+          services: services.map((service) => ({
             name: service.name,
             price: service.price,
             quantity: service.quantity,
-            })),
-            address,
-        
-}
-})
-if (!booking) return { error: "Booking not created" };
-return { success: true };
-    } catch (error) {
-        return { error: "Booking failed" };        
-    }
+          })),
+        },
+      });
+    });
+    if (!isbooking) return { error: "Booking not created" };
+    return { success: true };
+  } catch (error) {
+    return { error: "Booking failed" };
+  }
 }
