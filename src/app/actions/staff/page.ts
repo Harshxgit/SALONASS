636ee4cc6153a3e74b53cb1bfed53945e06cef79
Harshxgit <htp.codes@gmail.com>
2025-01-02 23:29:56 +0530

    pushed logic

--- src/app/actions/staff/page.ts
+++ src/app/actions/staff/page.ts
@@ -5,9 +5,14 @@ interface StaffAvailability{
     duration : string;
     datestr : Date
 }
+interface generateslots extends StaffAvailability{
+    starttime: string;
+    endtime: string;
+    booking: {starttime:string, endtime:string}[]
+}
 export async function getstafffavailablity({staffid, duration,datestr}:StaffAvailability) {
     
-    const staff = prisma.staff.findUnique({
+    const staff = await prisma.staff.findUnique({
         where: {
             id: staffid
         },
@@ -15,12 +20,67 @@ export async function getstafffavailablity({staffid, duration,datestr}:StaffAvai
             StaffAvailability: {
                 where: {
                     date: datestr
+                },
+                select:{
+                    isAvailable:true
+                }
+            },
+            booking:{
+                where:{
+                    date: datestr
+                },
+                select:{
+                    starttime:true,
+                    endtime:true
                 }
             }
         }
     })
 
+    if(!staff || !staff.StaffAvailability){ return []}
+        
+    const {StaffAvailability, booking} = staff
+    const availability = StaffAvailability?.map(avail => avail.isAvailable)
+
+    if(!availability){ return []}
+    if(availability){
+
+    }
     
+}
 
+function generateslots({
+    starttime,
+    endtime,
+    booking,
+    duration,
+    datestr
+}:generateslots){
 
+    const slots = []
+
+    // i m letting timegap logic here 
+    let currenttime = new Date(starttime)
+    let endtimeDate = new Date(endtime)
+    while(currenttime < endtimeDate){
+        const endslot = new Date(currenttime.getTime() + parseInt(duration) * 60000)
+
+        const isSlotAvailable = !booking.some(book => {
+            const bookstarttime = new Date(book.starttime)
+            const bookendtime = new Date(book.endtime)
+
+            return(
+                (currenttime >= bookstarttime && currenttime < bookendtime) ||
+                (endslot > bookstarttime && endslot <= bookendtime) ||
+                (currenttime < bookstarttime && endslot > bookendtime)
+            )
+
+    })
+    if(isSlotAvailable){
+        slots.push({
+            currenttime
+    })
+    }
+    
+}
 }
