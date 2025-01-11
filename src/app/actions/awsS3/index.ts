00bf52a5a50024c4cbf263cfa7cbc46e4ccbac52
Harshxgit <htp.codes@gmail.com>
2025-01-11 09:45:54 +0530

    fixed error

--- src/app/actions/awsS3/index.ts
+++ src/app/actions/awsS3/index.ts
@@ -1,8 +1,10 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { getSession } from "next-auth/react";
-import crypto from "crypto"
+import crypto from "crypto";
+import { promises } from "dns";
 
+//AWS S3 client connection
 const s3Client = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
@@ -10,43 +12,45 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
-type SignedURLResponse = Promise<
-  | { failure?: undefined; success: { url: string } }
-  | { failure: string; success?: undefined }
->;
+
 interface SignedUrlResult {
-    fileName: string;
-    uploadUrl: string;
-  }
-export async function getSignedURL(): SignedURLResponse {
+  fileName?: string;
+  uploadUrl?: string;
+  failure?: string;
+}
+
+//get signedURL function , with the help of we will taking signed url
+export async function getSignedURL(
+  numberOfFiles: number
+): Promise<SignedUrlResult[]> {
+  //take user session
   const session = await getSession();
   if (!session) {
-    return { failure: "not authenticated" };
+    return [{ failure: "not authenticated" }];
   }
 
-  const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex")
-  const signedUrl:SignedUrlResult[] =await Promise.all(
-    Array.from({length:number},asyn(__,index)=>{
-        
-        const putObjectCommand = new PutObjectCommand({
-          Bucket: process.env.AWS_BUCKET_NAME!,
-          Key: generateFileName()
-        });
-      
-        const url = await getSignedUrl(
-          s3Client,
-          putObjectCommand,
-          { expiresIn: 60 } // 60 seconds
-        );
-      
-      
-      
-      
-      
-        return { success: { url } };
+  //function generate automatic file name
+  const generateFileName = (bytes = 32) =>
+    crypto.randomBytes(bytes).toString("hex");
+
+  //signedURL function return array of object , used Array.from
+  const signedUrl: SignedUrlResult[] = await Promise.all(
+    Array.from({ length: numberOfFiles }, async (__, index) => {
+      const fileName = generateFileName();
+      const putObjectCommand = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME!,
+        Key: fileName,
+      });
+
+      const url = await getSignedUrl(
+        s3Client,
+        putObjectCommand,
+        { expiresIn: 60 } // 60 seconds
+      );
+
+      return { fileName, uploadUrl: url };
     })
+  );
 
-  )
-
-    return signedUrl
+  return signedUrl;
 }
